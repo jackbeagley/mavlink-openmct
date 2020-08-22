76f8ae4bb4eb7d375f382a416adb3cc3a48031bd
Jackson Beagley <jack.beagley@gmail.com>
2020-08-23 10:49:06 +1200

    Root folder displays

--- mavlink-telemetry.js
+++ mavlink-telemetry.js
@@ -1,12 +1,27 @@
-import mavlinkMessagesJSON from './telemetry-messages.json'
+import mavlinkMessagesDict from './telemetry-messages.json'
+
+var objectProvider = {
+    get: function (identifier) {
+        if (identifier.key === 'craft') {
+            return Promise.resolve({
+                identifier: identifier,
+                name: mavlinkMessagesDict.name,
+                type: 'folder',
+                location: 'ROOT'
+            })
+        }
+        }
+    };
 
 export default function () {
     return function install(openmct) {
         openmct.objects.addRoot({
-            namespace: 'mavlink_craft',
-            key: 'craft_telemetry'
+            namespace: 'mavlink.taxonomy',
+            key: 'craft'
         });
         
+        openmct.objects.addProvider('mavlink.taxonomy', objectProvider);
+
         var provider = {
             supportsSubscribe: function (domainObject) {
                 return domainObject.type === "mavlink.telemetry";
