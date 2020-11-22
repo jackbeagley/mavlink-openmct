252f019af50f0b003373e9aeb2262a58dc1709ca
Jackson Beagley <jack.beagley@gmail.com>
2020-11-22 16:47:40 +1300

    setup.js working!

---
+++ setup.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const { execSync } = require("child_process");
+const prompt = require('prompt-sync')({sigint: true});
+
+let pythonPath = __dirname + '/pymavlink';
+
+process.env.PYTHONPATH = pythonPath
+
+let options = {
+    stdio: [null, process.stdout, process.stderr],
+    env: process.env
+};
+
+let mavlinkXML = prompt('Please enter the xml you wish to use for MAVLink: ')
+
+while (!fs.existsSync(mavlinkXML)) {
+    mavlinkXML =prompt('Sorry, the file entered couldn\'t be found, please try again: ')
+}
+
+
+if (!fs.existsSync('assets')) {
+    fs.mkdirSync('assets');
+}
+
+execSync("echo %%PYTHONPATH%%", options);
+
+//console.log(output)
+
+execSync("py -m pymavlink.tools.mavgen -o ./assets --lang TypeScript --wire-protocol 2.0 " + mavlinkXML, options)
+//let output = exec("set PYTHONPATH=%cd%\pymavlink && echo PYTHONPATH for pymavlink set to: %PYTHONPATH% && mkdir assets & py -m pymavlink.tools.mavgen -o ./assets --lang TypeScript --wire-protocol 2.0 telemetry.xml && node ./node_modules/typescript/bin/tsc -p assets && node json-generator.js")
+
+//console.log(output)
